fa33c54f4ca823a9118e864bdc6bda9fbe1eba02
Muhammad Huzaifa Khan <huzaifanadir1997@gmail.com>
2021-12-05 01:52:10 +0500

    Explore Appbar, loader, Textfield, navbar

--- flutter_bricks_examples/lib/main.dart
+++ flutter_bricks_examples/lib/main.dart
@@ -1,113 +1,406 @@
 import 'package:flutter/material.dart';
+import 'dart:ui';
+import 'dart:math' as Math;
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  final demoController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        appBar: MallikaAppBar5(),
+        body: Center(
+          child: Column(
+            children: [
+              DottedCircularProgressIndicatorFb(
+                numDots: 10,
+                defaultDotColor: Colors.orange,
+                currentDotColor: Colors.red,
+              ),
+              EmailInputFb1(inputController: demoController),
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomNavBarMallika1(),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
+// APP BAR
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class MallikaAppBar5 extends StatelessWidget with PreferredSizeWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  final Size preferredSize;
+
+  MallikaAppBar5({Key? key})
+      : preferredSize = const Size.fromHeight(56.0),
+        super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+        backgroundColor: const Color(0xffFF8527),
+        elevation: 1,
+        leading: IconButton(
+          icon: const Icon(Icons.keyboard_arrow_left, color: Colors.white),
+          onPressed: () {},
+        ),
+        title: const Text(
+          "Recipe",
+          style: TextStyle(color: Colors.white),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.save, color: Colors.white),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(Icons.share, color: Colors.white),
+            onPressed: () {},
+          )
+        ]);
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+// CIRCULAT DOT LOADER
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class DottedCircularProgressIndicatorFb extends StatefulWidget {
+  const DottedCircularProgressIndicatorFb({
+    Key? key,
+    required this.currentDotColor,
+    required this.defaultDotColor,
+    required this.numDots,
+    this.size = 50,
+    this.dotSize = 5,
+    this.secondsPerRotation = 1,
+  }) : super(key: key);
+
+  final int numDots; //Number of dots around the dot circle
+  final double size; //Width and height of the
+  final double dotSize; //Diameter of each dot
+  final int secondsPerRotation;
+  final Color
+      currentDotColor; //The color of the dot that "circles" around the indicator
+  final Color defaultDotColor; //The color of the dots that aren't animated
+
+  @override
+  State<DottedCircularProgressIndicatorFb> createState() =>
+      _DottedCircularProgressIndicatorFbState();
+}
+
+class _DottedCircularProgressIndicatorFbState
+    extends State<DottedCircularProgressIndicatorFb>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animController;
+  late Animation<int> animation;
+
+  @override
+  void initState() {
+    super.initState();
+    animController = AnimationController(
+        duration: Duration(seconds: widget.secondsPerRotation), vsync: this)
+      ..repeat();
+    animation =
+        StepTween(begin: 0, end: widget.numDots + 1).animate(animController)
+          ..addListener(() {
+            setState(() {});
+          });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    animController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return Container(
+      width: widget.size,
+      height: widget.size,
+      // decoration: BoxDecoration(color: Colors.red), //for debugging purposes
+      child: CustomPaint(
+        child: Container(),
+        painter: DottedCircularProgressIndicatorPainterFb(
+            dotColor: widget.defaultDotColor,
+            currentDotColor: widget.currentDotColor,
+            percentage: 0,
+            numDots: widget.numDots,
+            currentDotNum: animation.value,
+            dotWidth: widget.dotSize),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    );
+  }
+}
+
+class DottedCircularProgressIndicatorPainterFb extends CustomPainter {
+  final double percentage;
+  final Color dotColor;
+  final Color currentDotColor;
+  final int numDots;
+  final int currentDotNum;
+  final double dotWidth;
+
+  DottedCircularProgressIndicatorPainterFb(
+      {required this.dotColor,
+      required this.currentDotColor,
+      required this.percentage,
+      required this.numDots,
+      required this.dotWidth,
+      required this.currentDotNum});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final double smallestSide = Math.min(size.width, size.height);
+    final double radius = (smallestSide / 2 - dotWidth / 2) - dotWidth / 2;
+    final Offset centerPoint = Offset(size.width / 2, size.height / 2);
+
+    final dotPaint = Paint()
+      ..style = PaintingStyle.fill
+      ..color = dotColor;
+
+    Circle myCircle = Circle(radius: radius, totalNumOfDots: numDots);
+    for (var i = 0; i < numDots; i++) {
+      if (i == currentDotNum) {
+        dotPaint.color = currentDotColor;
+      } else {
+        dotPaint.color = dotColor;
+      }
+      canvas.drawCircle(
+          centerPoint - myCircle.calcDotOffsetFromCenter(i, radius),
+          dotWidth,
+          dotPaint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(DottedCircularProgressIndicatorPainterFb oldDelegate) =>
+      oldDelegate.currentDotNum != currentDotNum ? true : false;
+}
+
+class Circle {
+  int totalNumOfDots;
+  double radius;
+
+  Circle({required this.totalNumOfDots, required this.radius});
+
+  Offset calcDotOffsetFromCenter(int dotNumber, double radius) {
+    return Offset(_calcDotX(dotNumber), _calcDotY(dotNumber));
+  }
+
+  double _calcDotAngle(int currentDotNumber) {
+    return -currentDotNumber * _calcRadiansPerDot();
+  }
+
+  double _calcRadiansPerDot() {
+    return 2 * Math.pi / (totalNumOfDots);
+  }
+
+  double _calcDotX(int thisDotNum) {
+    //offset from indicator circles center
+    return radius * Math.sin(_calcDotAngle(thisDotNum));
+  }
+
+  double _calcDotY(int thisDotNum) {
+    //offset from indicator circles center
+    return radius * Math.cos(_calcDotAngle(thisDotNum));
+  }
+}
+
+// INPUT FIELD
+class EmailInputFb1 extends StatelessWidget {
+  final TextEditingController inputController;
+  const EmailInputFb1({Key? key, required this.inputController})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const primaryColor = Color(0xff4338CA);
+    const secondaryColor = Color(0xff6D28D9);
+    const accentColor = Color(0xffffffff);
+    const backgroundColor = Color(0xffffffff);
+    const errorColor = Color(0xffEF4444);
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Email",
+          style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.normal,
+              color: Colors.white.withOpacity(.9)),
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        Container(
+          height: 50,
+          decoration: BoxDecoration(boxShadow: [
+            BoxShadow(
+                offset: const Offset(12, 26),
+                blurRadius: 50,
+                spreadRadius: 0,
+                color: Colors.grey.withOpacity(.1)),
+          ]),
+          child: TextField(
+            controller: inputController,
+            onChanged: (value) {
+              //Do something wi
+            },
+            keyboardType: TextInputType.emailAddress,
+            style: const TextStyle(fontSize: 14, color: Colors.black),
+            decoration: InputDecoration(
+              // prefixIcon: Icon(Icons.email),
+              filled: true,
+              fillColor: accentColor,
+              hintText: 'Enter your email',
+              hintStyle: TextStyle(color: Colors.grey.withOpacity(.75)),
+              contentPadding:
+                  const EdgeInsets.symmetric(vertical: 0.0, horizontal: 20.0),
+              border: const OutlineInputBorder(
+                borderSide: BorderSide(color: primaryColor, width: 1.0),
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+              ),
+              focusedBorder: const OutlineInputBorder(
+                borderSide: BorderSide(color: secondaryColor, width: 1.0),
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+              ),
+              errorBorder: const OutlineInputBorder(
+                borderSide: BorderSide(color: errorColor, width: 1.0),
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+              ),
+              enabledBorder: const OutlineInputBorder(
+                borderSide: BorderSide(color: primaryColor, width: 1.0),
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+// BOTTOM NAVBAR
+
+class BottomNavBarMallika1 extends StatelessWidget {
+  const BottomNavBarMallika1({Key? key}) : super(key: key);
+
+  final orangeColor = const Color(0xffFF8527);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      color: Colors.white,
+      child: SizedBox(
+        height: 56,
+        width: MediaQuery.of(context).size.width,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconBottomBar(
+                  text: "Home",
+                  icon: Icons.home_outlined,
+                  selected: true,
+                  onPressed: () {}),
+              IconBottomBar(
+                  text: "Search",
+                  icon: Icons.search_outlined,
+                  selected: false,
+                  onPressed: () {}),
+              IconBottomBar2(
+                  text: "Add",
+                  icon: Icons.add_outlined,
+                  selected: false,
+                  onPressed: () {}),
+              IconBottomBar(
+                  text: "Cart",
+                  icon: Icons.local_grocery_store_outlined,
+                  selected: false,
+                  onPressed: () {}),
+              IconBottomBar(
+                  text: "Calendar",
+                  icon: Icons.date_range_outlined,
+                  selected: false,
+                  onPressed: () {})
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class IconBottomBar extends StatelessWidget {
+  const IconBottomBar(
+      {Key? key,
+      required this.text,
+      required this.icon,
+      required this.selected,
+      required this.onPressed})
+      : super(key: key);
+  final String text;
+  final IconData icon;
+  final bool selected;
+  final Function() onPressed;
+
+  final orangeColor = const Color(0xffFF8527);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        IconButton(
+          onPressed: onPressed,
+          icon: Icon(
+            icon,
+            size: 25,
+            color: selected ? orangeColor : Colors.black54,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class IconBottomBar2 extends StatelessWidget {
+  const IconBottomBar2(
+      {Key? key,
+      required this.text,
+      required this.icon,
+      required this.selected,
+      required this.onPressed})
+      : super(key: key);
+  final String text;
+  final IconData icon;
+  final bool selected;
+  final Function() onPressed;
+  final orangeColor = const Color(0xffFF8527);
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(
+      backgroundColor: orangeColor,
+      child: IconButton(
+        onPressed: onPressed,
+        icon: Icon(
+          icon,
+          size: 25,
+          color: Colors.white,
+        ),
+      ),
     );
   }
 }
