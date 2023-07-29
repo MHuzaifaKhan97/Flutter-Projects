4667b13980ef9634160b4509d54788630027f5e6
Muhammad Huzaifa Khan <huzaifa.khan@tpsonline.com>
2023-07-29 20:19:18 +0500

    minor changes

--- ecomerce_app_ui/lib/main.dart
+++ ecomerce_app_ui/lib/main.dart
@@ -12,7 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Flutter Demo',
+      title: 'Ecomerce App UI',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
