25c7fc42aa4dc0d6a2ebc571ad7e0dd057155ede
Muhammad Huzaifa Khan <huzaifa.khan@tpsonline.com>
2023-08-20 16:13:50 +0500

    Packages added and basic app init

--- architecture_basics/lib/main.dart
+++ architecture_basics/lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
+import 'package:architecture_basics/user_json.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -19,11 +23,65 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final List<UserJson> users = [];
+
+  _fetchUsers() async {
+    var url = Uri.parse("https://jsonplaceholder.typicode.com/users");
+    var response = await http.get(url);
+
+    var list = jsonDecode(response.body) as List;
+    users.addAll(list.map((e) => UserJson.fromJson(e)).toList());
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchUsers();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("App Architecture Basics"),
+      ),
+      body: Center(
+        child: ListView(
+            padding: const EdgeInsets.only(top: 8),
+            children: users
+                .map(
+                  (user) => Container(
+                    margin:
+                        const EdgeInsets.symmetric(horizontal: 16, vertical: 6),
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade400,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: ListTile(
+                      title: Text(user.name ?? ""),
+                      subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(user.email ?? ""),
+                            Text(user.phone ?? ""),
+                            Text(user.website ?? ""),
+                          ]),
+                      leading: CircleAvatar(child: Text(user.name![0])),
+                    ),
+                  ),
+                )
+                .toList()),
+      ),
+    );
   }
 }
