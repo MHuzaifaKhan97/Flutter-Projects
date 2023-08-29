7a6d52df06554d762718f5e6424cba01b8c15883
Muhammad Huzaifa Khan <huzaifa.khan@tpsonline.com>
2023-08-29 18:11:08 +0500

    Background notification.

--- flutter_firebase_push_notifications/lib/main.dart
+++ flutter_firebase_push_notifications/lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase_push_notifications/home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
   runApp(const MyApp());
+}
+
+// This function should be the entry point of app
+@pragma("vm-entry-point")
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print(message.notification?.title?.toString());
 }
 
 class MyApp extends StatelessWidget {
