a5589c0a5df9be55af44a9ef58f24a7737b06c0e
Muhammad Huzaifa Khan <47402181+MHuzaifaKhan97@users.noreply.github.com>
2023-09-23 15:55:28 +0500

    Login Animation

--- animation_basics/lib/explicit_examples/login_animation.dart
+++ animation_basics/lib/explicit_examples/login_animation.dart
@@ -1,7 +1,33 @@
 import 'package:flutter/material.dart';
 
-class LoginScreenAnimation extends StatelessWidget {
+class LoginScreenAnimation extends StatefulWidget {
   const LoginScreenAnimation({super.key});
+
+  @override
+  State<LoginScreenAnimation> createState() => _LoginScreenAnimationState();
+}
+
+class _LoginScreenAnimationState extends State<LoginScreenAnimation>
+    with SingleTickerProviderStateMixin {
+  late Animation<double> logoFadeAnimation;
+  late AnimationController animationController;
+  late Animation<Offset> slideAnimation;
+  late Animation<double> scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 1000));
+    logoFadeAnimation =
+        Tween<double>(begin: 0, end: 1).animate(animationController);
+    slideAnimation =
+        Tween<Offset>(begin: const Offset(-1, -1), end: Offset.zero).animate(
+            CurvedAnimation(parent: animationController, curve: Curves.ease));
+    scaleAnimation =
+        Tween<double>(begin: 0, end: 1).animate(animationController);
+    animationController.forward();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,31 +36,40 @@ class LoginScreenAnimation extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const FlutterLogo(
-              size: 100.0,
+            FadeTransition(
+              opacity: logoFadeAnimation,
+              child: const FlutterLogo(
+                size: 100.0,
+              ),
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Column(
-                children: [
-                  const TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Username',
-                    ),
+              child: SlideTransition(
+                position: slideAnimation,
+                child: ScaleTransition(
+                  scale: scaleAnimation,
+                  child: Column(
+                    children: [
+                      const TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Username',
+                        ),
+                      ),
+                      const SizedBox(height: 16.0),
+                      const TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                        ),
+                        obscureText: true,
+                      ),
+                      const SizedBox(height: 16.0),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: const Text('Login'),
+                      ),
+                    ],
                   ),
-                  const SizedBox(height: 16.0),
-                  const TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Password',
-                    ),
-                    obscureText: true,
-                  ),
-                  const SizedBox(height: 16.0),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Text('Login'),
-                  ),
-                ],
+                ),
               ),
             ),
           ],
--- animation_basics/lib/main.dart
+++ animation_basics/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:animation_basics/implicit_examples/animated_tween_animation_builder_example.dart';
+import 'package:animation_basics/explicit_examples/login_animation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const PulsatingCircleAnimation(),
+      home: const LoginScreenAnimation(),
     );
   }
 }
