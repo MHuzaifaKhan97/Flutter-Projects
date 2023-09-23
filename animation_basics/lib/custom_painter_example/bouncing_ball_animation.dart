6cbea5787dd410b0c47a794e02154515c28560fb
Muhammad Huzaifa Khan <huzaifa.khan@tpsonline.com>
2023-09-23 18:53:02 +0500

    Custom paineter completed.

--- animation_basics/lib/custom_painter_example/bouncing_ball_animation.dart
+++ animation_basics/lib/custom_painter_example/bouncing_ball_animation.dart
@@ -7,7 +7,28 @@ class BouncingBallAnimation extends StatefulWidget {
   State<BouncingBallAnimation> createState() => _BouncingBallAnimationState();
 }
 
-class _BouncingBallAnimationState extends State<BouncingBallAnimation> {
+class _BouncingBallAnimationState extends State<BouncingBallAnimation>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<double> animation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    animation = Tween<double>(begin: 0, end: 1).animate(controller);
+
+    animation.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        controller.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        controller.forward();
+      }
+    });
+    controller.forward();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,9 +37,36 @@ class _BouncingBallAnimationState extends State<BouncingBallAnimation> {
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
             // Need Ball
+            AnimatedBuilder(
+                animation: animation,
+                builder: (context, child) {
+                  return CustomPaint(
+                    size: const Size(200, 200),
+                    painter: BouningBallPainter(
+                      animation.value,
+                    ),
+                  );
+                }),
           ],
         ),
       ),
     );
+  }
+}
+
+class BouningBallPainter extends CustomPainter {
+  final double animationValue;
+  BouningBallPainter(this.animationValue);
+  @override
+  void paint(Canvas canvas, Size size) {
+    canvas.drawCircle(
+        Offset(size.width / 2, size.height - (size.height * animationValue)),
+        20,
+        Paint()..color = Colors.blue);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
--- animation_basics/lib/main.dart
+++ animation_basics/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:animation_basics/page_route_builder_animation/splash_animation.dart';
+import 'package:animation_basics/custom_painter_example/bouncing_ball_animation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,6 +16,6 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: const SplashAnimation());
+        home: const BouncingBallAnimation());
   }
 }
