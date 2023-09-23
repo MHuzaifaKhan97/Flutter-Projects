d54f92040a86dd9cfaf2d498e5a155ac5f14eb1e
Muhammad Huzaifa Khan <47402181+MHuzaifaKhan97@users.noreply.github.com>
2023-09-23 14:25:39 +0500

    Animated cart button completed.

--- animation_basics/lib/implicit_examples/animated_shopping_cart_button.dart
+++ animation_basics/lib/implicit_examples/animated_shopping_cart_button.dart
@@ -1,25 +1,57 @@
 import 'package:flutter/material.dart';
 
-class ShoppingCartButton extends StatelessWidget {
+class ShoppingCartButton extends StatefulWidget {
   const ShoppingCartButton({super.key});
 
+  @override
+  State<ShoppingCartButton> createState() => _ShoppingCartButtonState();
+}
+
+class _ShoppingCartButtonState extends State<ShoppingCartButton> {
+  bool isAnimationExpanded = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Shopping Cart'),
+        centerTitle: true,
       ),
       body: Center(
-        child: Container(
-          width: 80.0,
-          height: 60.0,
-          decoration: BoxDecoration(
-            color: Colors.blue,
-            borderRadius: BorderRadius.circular(10.0),
-          ),
-          child: const Icon(
-            Icons.shopping_cart,
-            color: Colors.white,
+        child: GestureDetector(
+          onTap: () {
+            setState(() {
+              isAnimationExpanded = !isAnimationExpanded;
+            });
+          },
+          child: AnimatedContainer(
+            duration: const Duration(milliseconds: 800),
+            width: isAnimationExpanded ? 160.0 : 80.0,
+            height: 60.0,
+            decoration: BoxDecoration(
+              color: isAnimationExpanded ? Colors.green : Colors.blue,
+              borderRadius:
+                  BorderRadius.circular(isAnimationExpanded ? 40.0 : 10.0),
+            ),
+            child: isAnimationExpanded
+                ? Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Icon(
+                        Icons.check,
+                        color: Colors.white,
+                      ),
+                      SizedBox(width: 4),
+                      Text(
+                        "Added to Cart",
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  )
+                : const Icon(
+                    Icons.shopping_cart,
+                    color: Colors.white,
+                  ),
           ),
         ),
       ),
--- animation_basics/lib/main.dart
+++ animation_basics/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:animation_basics/implicit_examples/animated_color_pallete.dart';
+import 'package:animation_basics/implicit_examples/animated_shopping_cart_button.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,12 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Animations',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const AnimatedColorPalette(),
+      home: const ShoppingCartButton(),
     );
   }
 }
